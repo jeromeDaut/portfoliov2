a7047ed2f7bb6c800d9ef7d6e907d10c7daa1178
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-17 22:40:00 +0200

    onclick

--- src/components/projectCards/Cards.jsx
+++ src/components/projectCards/Cards.jsx
@@ -1,36 +1,42 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import image from '../../assets/images/image1.jpg';
-
+// import './Cards.scss';
 
 const Cards = () => {
-    const [cardsData, setCardsData] = useState([]);
+  const [cardsData, setCardsData] = useState([]);
+  const [showCards, setShowCards] = useState(false);
+  
 
-    useEffect(() => {
-      fetch('/cards.json')
-        .then(response => response.json())
-        .then(data => setCardsData(data))
-        .catch(error => console.error(error));
-    }, []);
+  useEffect(() => {
+    fetch('/cards.json')
+      .then(response => response.json())
+      .then(data => setCardsData(data))
+      .catch(error => console.error(error));
+  }, []);
 
-    return (
-    <section className='project' >
-        <h2 className="project__title">Mes projets</h2>
-        <div className='container'>
-            <div>
-                <img className='container__image' src={image} alt="" />
-            </div>
-            <div className='container__project'>
-                {cardsData.map(card => (
-                <div className="card" key={card.id}>
-                    <img className="card__image" src={card.cover} alt="" />
-                    <h3 className="card__title">{card.title}</h3>
-                    <p className="card__description">{card.paragraph}</p>
-                </div>
-            ))}
-            </div>
+  const handleClick = () => {
+    setShowCards(!showCards);
+  };
+
+  return (
+    <section className='project'>
+      <h2 className='project__title'>Mes projets</h2>
+      <div className='container'>
+        <div className={`container__image ${showCards ? 'shifted' : ''}`} onClick={handleClick}>
+          <img src={image} alt='' />
         </div>
+        {showCards && (
+          <div className='container__project'>
+            {cardsData.map(card => (
+              <div className='card' key={card.id}>
+                <img className='card__image' src={card.cover} alt='' />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </section>
-    );
+  );
 };
 
 export default Cards;
