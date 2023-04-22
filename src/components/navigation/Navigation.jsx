fd5541a7a695f86ee99d45475e3a3a946b4510b3
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-22 19:46:50 +0200

    modification nav avec hash-link

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const Navigation = () => {
     return (
         <header className="header">
         <div className="header__nav">
           <ul className="header__nav-list">
-            <li className="header__nav-item"><Link to="/" className="header__nav-link">A propos</Link></li>
-            <li className="header__nav-item"><Link to="/" className="header__nav-link">Mes Projets</Link></li>
-            <li className="header__nav-item"><Link to="/" className="header__nav-link">Contact</Link></li>
+            <li className="header__nav-item"><HashLink to="#about"  className="header__nav-link">A propos</HashLink></li>
+            <li className="header__nav-item"><HashLink to="#skills"  className="header__nav-link">Compétences</HashLink></li>
+            <li className="header__nav-item"><HashLink to="#project" className="header__nav-link">Projet</HashLink></li>
+            <li className="header__nav-item"><HashLink to="#contact" className="header__nav-link">Contact</HashLink></li>
           </ul>
         </div>
       </header>
