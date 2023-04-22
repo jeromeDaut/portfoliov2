c5094b5b4002a69ac98249463a92151046b6d906
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-22 19:48:02 +0200

    ajout des composants dans home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Navigation from '../components/navigation/Navigation';
 import About from '../components/AboutMe/About';
 import Cards from '../components/projectCards/Cards';
+import Skills from '../components/skills/Skills';
+import Form from '../components/Form/Form';
+import Footer from '../components/footer/Footer';
 
 const Home = () => {
     return (
@@ -9,8 +12,11 @@ const Home = () => {
             <Navigation />
             <main>
             <About/>
+            <Skills />
             <Cards/>
+            <Form />
             </main>
+            <Footer />
         </>
     );
 };
