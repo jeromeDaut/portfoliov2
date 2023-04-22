a5845b8ca10982073b124c1bc782ff7c6b7db3cc
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-22 19:45:39 +0200

    création du footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <>
+            <div className="contactR">
+                <Link to='#' onClick={()=>{window.location= 'mailto:jerome.dautreme@live.fr'}} className="contactR__reseaux"><i class='bx bx-envelope' ></i></Link>
+                <NavLink target='blank' to='https://github.com/jeromeDaut' className="contactR__reseaux"><i class='bx bxl-github' ></i></NavLink>
+                <NavLink target='blank' to='https://www.linkedin.com/in/j%C3%A9r%C3%B4me-dautr%C3%AAme-612168156/' className="contactR__reseaux"><i class='bx bxl-linkedin' ></i></NavLink>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
