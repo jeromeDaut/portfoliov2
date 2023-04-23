eb536aa80e763bb5cabbdf1ea59100c28e3cfaee
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-23 18:54:26 +0200

    correction

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
           <ul className="header__nav-list">
             <li className="header__nav-item"><HashLink to="#about"  className="header__nav-link">A propos</HashLink></li>
             <li className="header__nav-item"><HashLink to="#skills"  className="header__nav-link">Compétences</HashLink></li>
-            <li className="header__nav-item"><HashLink to="#project" className="header__nav-link">Projet</HashLink></li>
+            <li className="header__nav-item"><HashLink to="#project" className="header__nav-link">Projets</HashLink></li>
             <li className="header__nav-item"><HashLink to="#contact" className="header__nav-link">Contact</HashLink></li>
           </ul>
         </div>
