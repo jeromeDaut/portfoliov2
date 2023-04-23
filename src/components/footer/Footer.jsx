0a3de8f169a71744bc8fe16560c31011b8d5a563
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-23 18:53:14 +0200

    ajout style et animation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <>
             <div className="contactR">
-                <Link to='#' onClick={()=>{window.location= 'mailto:jerome.dautreme@live.fr'}} className="contactR__reseaux"><i class='bx bx-envelope' ></i></Link>
-                <NavLink target='blank' to='https://github.com/jeromeDaut' className="contactR__reseaux"><i class='bx bxl-github' ></i></NavLink>
-                <NavLink target='blank' to='https://www.linkedin.com/in/j%C3%A9r%C3%B4me-dautr%C3%AAme-612168156/' className="contactR__reseaux"><i class='bx bxl-linkedin' ></i></NavLink>
+                <Link to='#' onClick={()=>{window.location= 'mailto:jerome.dautreme@live.fr'}} className="contactR__reseaux"><i className='bx bx-envelope' ></i></Link>
+                <NavLink target='blank' to='https://github.com/jeromeDaut' className="contactR__reseaux"><i className='bx bxl-github' ></i></NavLink>
+                <NavLink target='blank' to='https://www.linkedin.com/in/j%C3%A9r%C3%B4me-dautr%C3%AAme-612168156/' className="contactR__reseaux"><i className='bx bxl-linkedin' ></i></NavLink>
             </div>
         </>
     );
