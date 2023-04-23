ca644dbec83ad07a2514b48bfa898cdb4d69c90b
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-23 18:53:34 +0200

    label et style

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import SectionTitle from "../sectionTitle/SectionTitle";
 
 function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -8,29 +9,27 @@ function Form() {
   
   return (
     <section className="formulaire">
-        <div className="formulaire__banner" id="contact">
-            <h2>Contact</h2>
-        </div>
+        <SectionTitle id="contact"/>
         <form onSubmit={handleSubmit(onSubmit)} className="form">
             <div className="form__row">
                
                 <div className="form__element form__element--half-width">
                     
                     <div className="form__field">
-                        <label className="form__label">Nom:</label>
-                        <input type="text" {...register("nom", {required: true})} className="form__input" />
+                        <label for="nom" className="form__label">Nom:</label>
+                        <input type="text" {...register("nom", {required: true})} className="form__input" name="nom" id="nom"/>
                         {errors.nom && <span className="form__error">Ce champ est requis.</span>}
                     </div>
 
                     <div className="form__field">
-                        <label className="form__label">Prénom:</label>
-                        <input type="text" {...register("prenom", {required: true})} className="form__input" />
+                        <label for="prenom" className="form__label">Prénom:</label>
+                        <input type="text" {...register("prenom", {required: true})} className="form__input" name="prenom" id="prenom" />
                         {errors.prenom && <span className="form__error">Ce champ est requis.</span>}
                     </div>
 
                     <div className="form__field">
-                        <label className="form__label">Adresse mail:</label>
-                        <input type="email" {...register("email", {required: true})} className="form__input" />
+                        <label for="mail" className="form__label">Adresse mail:</label>
+                        <input type="email" {...register("email", {required: true})} className="form__input" name="mail" id="mail"/>
                     {   errors.email && <span className="form__error">Ce champ est requis.</span>}
                     </div>
 
@@ -39,8 +38,8 @@ function Form() {
                 <div className="form__element form__element--half-width">
 
                     <div className="form__field">
-                        <label className="form__label">Message:</label>
-                        <textarea {...register("message", {required: true})} className="form__textarea" />
+                        <label for="text" className="form__label">Message:</label>
+                        <textarea {...register("message", {required: true})} className="form__textarea" name="text" id="text"/>
                         {errors.message && <span className="form__error">Ce champ est requis.</span>}
                     </div>
 
