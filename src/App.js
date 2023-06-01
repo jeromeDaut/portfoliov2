19ba8cc68dabc9fe60e0595a64fab46f0976b78f
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-01 15:48:21 +0200

    modification route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 function App() {
+  const errorOccurred = false;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        {errorOccurred && <Navigate to="*" />}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
