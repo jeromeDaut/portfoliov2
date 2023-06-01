a4e8b7449f61dcc90a2e8fbfa8edcee4c9c3c400
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-01 15:50:21 +0200

    création le la page 404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import shadok from '../assets/images/Shadok.webp'
 
 const PageNotFound = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <main >
+                <div className="error">
+                    <h1 className="error__title"><span className="error__title--404">Erreur: 404</span></h1>
+                    <p  className="error__title--msg">La page que vous cherchez n'existe pas.</p>
+                    <p className='error__shadok'>Mais pendant ce temps là, les shadoks pompaient, pompaient...</p>
+                    <img src={shadok} alt="" />
+                    <NavLink to="/" className="error__redirect">Retour à la page principale ?</NavLink>
+                </div>
+            </main>
+        </>
     );
 };
 
