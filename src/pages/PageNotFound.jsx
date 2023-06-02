7cff82729a988658f7dcf61355550b910a22d89f
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-02 07:32:39 +0200

    modification texte

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -9,7 +9,7 @@ const PageNotFound = () => {
                 <div className="error">
                     <h1 className="error__title"><span className="error__title--404">Erreur: 404</span></h1>
                     <p  className="error__title--msg">La page que vous cherchez n'existe pas.</p>
-                    <p className='error__shadok'>Mais pendant ce temps là, les shadoks pompaient, pompaient...</p>
+                    <p className='error__shadok'>Pendant ce temps là, les shadoks pompaient, pompaient...</p>
                     <img src={shadok} alt="" />
                     <NavLink to="/" className="error__redirect">Retour à la page principale ?</NavLink>
                 </div>
