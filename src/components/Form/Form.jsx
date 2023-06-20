fde83ec6fd8a9c5bed2dcdab095b31eac7c175e2
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-20 08:47:57 +0200

    ajout de emailjs rendant fonctionnel le formulaire

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useRef} from "react";
 import { useForm } from "react-hook-form";
+import emailjs from '@emailjs/browser';
 import SectionTitle from "../sectionTitle/SectionTitle";
 
 
 function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  
-  const onSubmit = data => console.log(data);
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_omiyaph', 'template_sx62q5j', form.current, 'OIDXRZBCHs5rksSz0')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   
   return (
     <section className="formulaire">
         <SectionTitle id="contact"/>
-        <form method="post" onSubmit={handleSubmit(onSubmit)} className="form" action="../../../submit_form.php">
+        <form ref={form} method="post" onSubmit={sendEmail} className="form" action="../../../submit_form.php">
             <div className="form__row">
                
                 <div className="form__element form__element--half-width">
