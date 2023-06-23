50120b59695e73831a9fe8b2c225915e1e598ab0
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-23 07:49:10 +0200

    ajout téléchargement CV

--- src/components/AboutMe/About.jsx
+++ src/components/AboutMe/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import portrait from '../../assets/images/portrait.webp';
 import SectionTitle from '../sectionTitle/SectionTitle';
+import { FaRegFilePdf } from 'react-icons/fa';
+import pdf from '../../assets/images/CV_JEROME_DAUTREME.pdf';
+
 
 const About = () => {
+  const handleDownload = () => {
+    window.open(pdf, '_blank');
+  };
 
   return (
     <>
@@ -20,6 +26,8 @@ const About = () => {
     
         <aside className="about-me__text">
           <p>&nbsp;&nbsp;Actuellement en reconversion professionnelle dans le développement web front-end. Après une carrière réussie en tant que chef de cuisine, j'ai décidé de suivre ma passion pour la programmation. J'ai suivi avec succès une formation de neuf mois chez OpenClassrooms, au cours de laquelle j'ai acquis des compétences solides en HTML, CSS, JavaScript, React, ainsi que d'autres compétences telles que le greenCode, le SEO et les méthodes Agile.<br/><br/>&nbsp;&nbsp;Je suis à la recherche d'une entreprise accueillante pour effectuer une alternance, afin de mettre en pratique mes connaissances et d'acquérir une expérience concrète auprès d'experts du domaine. Passionné, déterminé et avide de défis, je suis convaincu que ma transition réussie et ma combinaison unique d'expériences feront de moi un atout précieux pour toute équipe de développement. Je suis impatient de contribuer à des projets innovants et de continuer à grandir en tant que professionnel.</p>
+          <br />
+          <button className='btn_download ' onClick={handleDownload}><p>Télécharger mon CV :<FaRegFilePdf className='pdf'/></p> </button>
         </aside>
     </section>
     </>
