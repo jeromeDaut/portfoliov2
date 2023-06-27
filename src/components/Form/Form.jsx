c603ec8602403e81ddc6bd70854394eaafaabbd8
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-27 09:30:26 +0200

    formulaire avec formspee

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,40 +1,70 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 import SectionTitle from "../sectionTitle/SectionTitle";
-import sgMail from '@sendgrid/mail';
+import axios from 'axios';
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function Form() {
-    const [formData, setFormData] = useState({
-        nom: '',
-        prenom: '',
-        email: '',
-        message: ''
-      });
-    
-      function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
+  
+    const [status, setStatus] = useState({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+    const [inputs, setInputs] = useState({
+      email: '',
+      message: '',
+      prenom: '',
+      nom: '',
+    });
+    const handleServerResponse = (ok, msg) => {
+      if (ok) {
+        setStatus({
+          submitted: true,
+          submitting: false,
+          info: { error: false, msg: msg },
+        });
+        setInputs({
+          email: '',
+          message: '',
+          prenom: '',
+          nom: '',
+        });
+      } else {
+        setStatus({
+          info: { error: true, msg: msg },
+        });
       }
-    
-     async function handleSubmit(event) {
-        event.preventDefault();
-
-        const msg = {
-          to: 'jerome.dautreme@live.fr',
-          from: formData.email,
-          subject: `Nouveau message de ${formData.nom} ${formData.prenom}`,
-          text: formData.message
-        };
-        try {
-            await sgMail.send(msg);
-            setFormData({ nom: "", prenom: "", email: "", message: "" });
-            console.log("Email sent successfully");
-          } catch (error) {
-            console.error("Error sending email:", error);
-          }
-        }
+    };
+    const handleOnChange = (e) => {
+      e.persist();
+      setInputs((prev) => ({
+        ...prev,
+        [e.target.id]: e.target.value,
+      }));
+      setStatus({
+        submitted: false,
+        submitting: false,
+        info: { error: false, msg: null },
+      });
+    };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+      axios({
+        method: 'POST',
+        url: 'https://formspree.io/' + process.env.REACT_APP_FORMSPREE_KEY,
+        data: inputs,
+      })
+        .then((response) => {
+          handleServerResponse(
+            true,
+            'Thank you, your message has been submitted.',
+          );
+        })
+        .catch((error) => {
+          handleServerResponse(false, error.response.data.error);
+        });
+    };
   return (
     <section className="formulaire">
         <SectionTitle id="contact"/>
@@ -45,23 +75,45 @@ function Form() {
                     
                     <div className="form__field">
                         <label htmlFor="nom" className="form__label">Nom:</label>
-                        <input type="text" name="nom" value={formData.nom} onChange={handleInputChange} className="form__input" />
-                        {/* Error message if it's required */}
-                        {/* {errors.nom && <span className="form__error">Ce champ est requis.</span>} */}
+                        <input 
+                          type="text" 
+                          name="nom"
+                          id="nom" 
+                          className="form__input"
+                          onChange={handleOnChange}
+                          required
+                          value={inputs.nom}
+                        />
+                        
+                        
                     </div>
 
                     <div className="form__field">
                         <label htmlFor="prenom" className="form__label">Prénom:</label>
-                        <input type="text" name="prenom" value={formData.prenom} onChange={handleInputChange} className="form__input" />
-                        {/* Error message if it's required */}
-                        {/* {errors.prenom && <span className="form__error">Ce champ est requis.</span>} */}
+                        <input 
+                          type="text" 
+                          name="prenom"  
+                          className="form__input" 
+                          id="prenom"
+                          onChange={handleOnChange}
+                          required
+                          value={inputs.prenom}  
+                        />
+
+                        
                     </div>
 
                     <div className="form__field">
-                        <label htmlFor="mail" className="form__label">Adresse mail:</label>
-                        <input type="email" name="email" value={formData.email} onChange={handleInputChange} className="form__input" />
-                        {/* Error message if it's required */}
-                    {/* {   errors.email && <span className="form__error">Ce champ est requis.</span>} */}
+                        <label htmlFor="email" className="form__label">Adresse mail:</label>
+                        <input
+                         id="email" 
+                         type="email" 
+                         name="_replyto" 
+                         className="form__input"
+                         onChange={handleOnChange}
+                          required
+                          value={inputs.email}
+                        />
                     </div>
 
                 </div>
@@ -70,17 +122,32 @@ function Form() {
                     
                     <div className="form__field">
                         <label htmlFor="text" className="form__label">Message:</label>
-                        <textarea name="message" value={formData.message} onChange={handleInputChange} className="form__textarea" />
-                        {/* Error message if it's required */}
-                        {/* {errors.message && <span className="form__error">Ce champ est requis.</span>} */}
+                        <textarea 
+                          name="message" 
+                          className="form__textarea" 
+                          id="message"
+                          onChange={handleOnChange}
+                          required
+                          value={inputs.message}
+                        />
                     </div>
 
                 </div>
-                <button type="submit" className="form__submit">Envoyer</button>
+                <button type="submit" className="form__submit" disabled={status.submitting}>
+                  {!status.submitting
+                  ? !status.submitted
+                    ? 'Soumettre'
+                    : 'Envoyé !'
+                  : 'Envoie...'}
+                </button>
 
             </div>
 
         </form>
+        {status.info.error && (
+          <div className="error">Error: {status.info.msg}</div>
+        )}
+        {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
     </section>
   );
 }
